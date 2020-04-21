bfc61f001bdf3ee914bdd1e83a275ab0744e440d
kedar16 <kedar16@gwmail.gwu.edu>
2020-04-20 20:59:45 -0400

    Created different fragments, navigation components and android x

--- pricealertapp/src/main/java/kedar/com/pricealertapp/activities/MainActivity.kt
+++ pricealertapp/src/main/java/kedar/com/pricealertapp/activities/MainActivity.kt
@@ -1,121 +1,39 @@
 package kedar.com.pricealertapp.activities
 
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.TextView
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kedar.AlertApplication
-import kedar.com.pricealertapp.models.AlertSetUp
-import kedar.com.pricealertapp.models.Magnitude
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kedar.com.pricealertapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var tv: TextView? = null
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-//        if(AlertApplication.instance.mBound){
-//            startListening()
-//        }
+    private lateinit var navController: NavController
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.mainNavFragment)
+        val bottomTabs = HashSet<Int>().apply {
+            add(R.id.navigation_track)
+            add(R.id.navigation_dashboard)
+        }
+        appBarConfiguration = AppBarConfiguration.Builder(bottomTabs).setDrawerLayout(drawerLayout).build()
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        hideBackButton()
+        tabs.setupWithNavController(navController)
     }
 
-
-
-    private fun startListening(){
-        AlertApplication.instance.mService.createNewAlerts(mutableListOf(AlertSetUp("AAPL", 230.toBigDecimal(), Magnitude.POSITIVE, 1)
-                , AlertSetUp("MSFT", 160.toBigDecimal(), Magnitude.POSITIVE, 2),
-                AlertSetUp("BA", 130.toBigDecimal(), Magnitude.POSITIVE, 3)))
+    private fun hideBackButton() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        supportActionBar?.setDisplayShowHomeEnabled(false)
     }
-
-//    private lateinit var mService: MyRealTimeService
-//    private var mBound: Boolean = false
-//
-//    private val connectionEstablisher = object : ConnectionEstablisher{
-//        override fun connectionStarted(data: LiveData<String>) {
-//            data.observe(this@MainActivity, Observer {
-//                price -> tv!!.text = price
-//                val priceInBigDecimal = price?.toBigDecimalOrNull()
-//                if(priceInBigDecimal != null ){
-//                    if(priceInBigDecimal > 7100.000.toBigDecimal()){
-//                        createNotification("Price of BTC is now $priceInBigDecimal")
-//                    }
-//                }
-//            })
-//        }
-//    }
-//
-//    /** Defines callbacks for service binding, passed to bindService()  */
-//    private val connection = object : ServiceConnection {
-//
-//        override fun onServiceConnected(className: ComponentName, service: IBinder) {
-//            // We've bound to LocalService, cast the IBinder and get LocalService instance
-//            val binder = service as MyRealTimeService.LocalBinder
-//            mService = binder.getService()
-//            binder.setConnectionEstablished(connectionEstablisher)
-//            mBound = true
-//            startListening()
-//        }
-//
-//        override fun onServiceDisconnected(arg0: ComponentName) {
-//            mBound = false
-//        }
-//    }
-
-//    override fun onStop() {
-//        super.onStop()
-//        unbindService(connection)
-//        mBound = false
-//    }
-//
-//
-//    override fun onStart() {
-//        super.onStart()
-//        // Bind to LocalService
-//        Intent(this, MyRealTimeService::class.java).also { intent ->
-//            bindService(intent, connection, Context.BIND_AUTO_CREATE)
-//        }
-//    }
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//        tv = findViewById(R.id.price)
-//    }
-//
-//    private fun startListening(){
-//        mService.createNewAlerts(mutableListOf(AlertSetUp("AAPL", 230.toBigDecimal(), Magnitude.POSITIVE, 1)
-//        , AlertSetUp("MSFT", 160.toBigDecimal(), Magnitude.POSITIVE, 2),
-//                AlertSetUp("BA", 130.toBigDecimal(), Magnitude.POSITIVE, 3)))
-//    }
-//
-//    private fun createNotification(text: String){
-//        createNotificationChannel()
-//        val builder = NotificationCompat.Builder(this, "stock_notification_alert")
-//                .setSmallIcon(R.drawable.ic_launcher_background)
-//                .setContentTitle("Stock price alert")
-//                .setContentText(text)
-//                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-//
-//        with(NotificationManagerCompat.from(this)) {
-//            // notificationId is a unique int for each notification that you must define
-//            notify(52, builder.build())
-//        }
-//    }
-//
-//    private fun createNotificationChannel() {
-//        // Create the NotificationChannel, but only on API 26+ because
-//        // the NotificationChannel class is new and not in the support library
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            val name = "Stock notification channel"
-//            val descriptionText = "This is where you get up to date stock info."
-//            val importance = NotificationManager.IMPORTANCE_DEFAULT
-//            val channel = NotificationChannel("stock_notification_alert", name, importance).apply {
-//                description = descriptionText
-//            }
-//            // Register the channel with the system
-//            val notificationManager: NotificationManager =
-//                    getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//            notificationManager.createNotificationChannel(channel)
-//        }
-//    }
 
 }
